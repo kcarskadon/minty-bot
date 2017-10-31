3b4a5ff156be1fb5f70e4158a3e64d9c001ff2d4
Kenneth Carskadon <kenneth@iywt.com>
2017-10-31 16:51:39 -0700

    Reverting changes

--- bot.js
+++ bot.js
@@ -6,23 +6,9 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-      var args = message.substring(1).split(' ');
-      var cmd = args[0];
-
-      args = args.splice(1);
-      switch(cmd) {
-          // !ping
-          case 'greet':
-              bot.sendMessage({
-                  to: channelID,
-                  message: 'hello!'
-              });
-          break;
-          // Just add any case commands if you want to..
-       }
-    }
+  if (message.content === '!hello') {
+    message.reply('hello!');
+  }
 });
 
 client.login(process.env.BOT_TOKEN);
