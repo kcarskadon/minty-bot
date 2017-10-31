7835cd3f862a0eaff0288a4bfda7e10913698683
Kenneth Carskadon <kenneth@iywt.com>
2017-10-31 16:45:04 -0700

    Updated Switch case for more complex responses

--- bot.js
+++ bot.js
@@ -6,9 +6,23 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-  if (message.content === 'hello') {
-    message.reply('hello!');
-  }
+    // It will listen for messages that will start with `!`
+    if (message.substring(0, 1) == '!') {
+      var args = message.substring(1).split(' ');
+      var cmd = args[0];
+
+      args = args.splice(1);
+      switch(cmd) {
+          // !ping
+          case 'greet':
+              bot.sendMessage({
+                  to: channelID,
+                  message: 'hello!'
+              });
+          break;
+          // Just add any case commands if you want to..
+       }
+    }
 });
 
 client.login(process.env.BOT_TOKEN);
