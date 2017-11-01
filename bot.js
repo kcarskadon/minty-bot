968489f4bcdb28c26a0af509e68c8f3eb1c1e398
Kenneth Carskadon <kenneth@iywt.com>
2017-10-31 21:04:28 -0700

    added status command

--- bot.js
+++ bot.js
@@ -7,6 +7,9 @@ client.on('ready', () => {
 
 var prefix = "!minty"
 client.on('message', message => {
+    let args = message.content.split(' ').slice(1);
+    var argresult = args.join(' ');
+
     //minty will ignore any message that is not addressed to her
     if (!message.content.startsWith(prefix)) return;
 
@@ -16,6 +19,11 @@ client.on('message', message => {
     //basic response cases
     if (message.content.startsWith(prefix + ' greet')) {
         message.reply('Hello!');
+    } else
+
+    if (message.content.startsWith(prefix + ' setstatus')) {
+        if(!argresult) argresult = 'online';
+        client.user.setStatus(argresult);
     }
 });
 
