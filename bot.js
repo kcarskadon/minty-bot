d25c158d96dc6ea0e3c5c178a1b644717f67fd98
Kenneth Carskadon <kenneth@iywt.com>
2017-10-31 18:59:34 -0700

    include statement for bot to ignore messages not addressed to her

--- bot.js
+++ bot.js
@@ -2,14 +2,21 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log('I am ready!');
+    console.log('I am ready!');
 });
 
-var prefix = "!Minty"
+var prefix = "!minty"
 client.on('message', message => {
-  if (message.content.startsWith (prefix)) {
-      message.reply('Hello!');
-  }
+    //minty will ignore any message that is not addressed to her
+    if (!message.content.startsWith(prefix)) return;
+
+    //prevent minty from responding to herself
+    if (message.author === client.user) return;
+
+    //basic response cases
+    if (message.content.startsWith(prefix)) {
+        message.reply('Hello!');
+    }
 });
 
 client.login(process.env.BOT_TOKEN);
