c12b4db2edaacba44bde544aa03761ad56dcc995
Kenneth Carskadon <kenneth@iywt.com>
2017-10-31 20:55:43 -0700

    moved variable declarations

--- bot.js
+++ bot.js
@@ -9,12 +9,10 @@ var prefix = "!minty"
 client.on('message', message => {
     //minty will ignore any message that is not addressed to her
     if (!message.content.startsWith(prefix)) return;
-
-    //prevent minty from responding to herself
-    if (message.author.bot) return;
-
     let args = message.content.split(' ').slice(1);
     var argresult = args.join(' ');
+    //prevent minty from responding to herself
+    if (message.author.bot) return;
 
     //basic response cases
 
